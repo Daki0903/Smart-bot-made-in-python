866a64293db0ba5fcc534b4b57040ef523995920
Daki0903 <nesicking@gmail.com>
2025-05-05 23:16:43 +0200

    Update main.py

--- smartBot/main.py
+++ smartBot/main.py
@@ -3,10 +3,10 @@ from search import search_web, extract_link
 import webbrowser
 
 print("=== SmartBot ===")
-print("Odaberi režim rada:")
-print("1. Internet pretraga")
-print("2. Ćaskanje i učenje")
-print("3. Izlaz")
+print("Select the operating mode:")
+print("1. internet search)
+print("2. Chatting and learning")
+print("3. Exit")
 
 mode = input("Unesi broj režima: ").strip()
 
@@ -22,17 +22,17 @@ if mode == "1":
         # Opcija za otvaranje linka
         link = extract_link(query)
         if link:
-            otvori = input("Želiš da otvorim link? (da/ne): ").strip().lower()
-            if otvori == "da":
+            otvori = input("Do you want me to open the link? (yes/no): ").strip().lower()
+            if otvori == "yes":
                 webbrowser.open(link)
 
 elif mode == "2":
-    print("\nSmartBot [Ćaskanje]: Pitaj me bilo šta.")
+    print("\nSmartBot [Chat]: Ask me anything.")
     while True:
-        user_input = input("Ti: ").strip()
-        if user_input.lower() in ["izlaz", "kraj", "exit"]:
+        user_input = input("You: ").strip()
+        if user_input.lower() in ["Exit", "end", "exit"]:
             break
         response = get_smart_response(user_input)
         print("SmartBot:", response)
 else:
-    print("Doviđenja!")
+    print("Goodbye!")
